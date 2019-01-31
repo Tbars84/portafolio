4db022443a697ae5e3223f51ab3960f7267f02b3
{camilotabares} <{camilo.tabares.84@gmail.com}>
2019-01-31 10:05:13 -0500

    Subiendo cambios de feature

--- src/app/landing-container/landing-container.module.ts
+++ src/app/landing-container/landing-container.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingHome } from './main-components/landing-home/landing-home.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
+import { PerfilComponent } from './pages/perfil/perfil.component';
 
 @NgModule({
   declarations: [
     LandingHome,
-    InicioComponent
+    InicioComponent,
+    PerfilComponent
   ],
   imports: [
     CommonModule
